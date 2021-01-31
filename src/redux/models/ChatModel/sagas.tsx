798ccd7824216db30b86ce8ee154346321539e09
tomryb <1rybak.tomasz@gmail.com>
2021-01-31 13:55:18 +0100

    change name in sagas

--- src/redux/models/ChatModel/sagas.tsx
+++ src/redux/models/ChatModel/sagas.tsx
@@ -3,17 +3,17 @@ import { put, fork, takeEvery, delay } from 'redux-saga/effects';
 import get from 'lodash/get';
 import { ChatActions } from './types';
 import { PartialPayloadAction } from 'types/reduxTypes';
-import { setIsTemplateWorkingSuccess } from 'redux/models/TemplateModel/actions';
+import { setIsChatWorkingSuccess } from './actions';
 
 export const TemplateModelSagas = [
   function* () {
     yield fork(function* () {
-      yield takeEvery(ChatActions.SET_IS_CHAT_WORKING, setIsTemplateWorking);
+      yield takeEvery(ChatActions.SET_IS_CHAT_WORKING, setIsChatWorking);
     });
   }
 ];
 
-export function* setIsTemplateWorking({ payload }: PartialPayloadAction<{
+export function* setIsChatWorking({ payload }: PartialPayloadAction<{
   value: boolean
 }>) {
   try {
@@ -24,7 +24,7 @@ export function* setIsTemplateWorking({ payload }: PartialPayloadAction<{
 
       yield delay(2000); // wait 2 seconds
 
-      yield put(setIsTemplateWorkingSuccess());
+      yield put(setIsChatWorkingSuccess());
     } else {
       throw new Error('Something went terribly wrong :|');
     }
